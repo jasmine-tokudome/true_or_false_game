5672e71df8d2fb2acd357f226bf21df955216c2b
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-05-16 22:33:28 +0900

    modeが切り替わった時の処理を追加

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ function judge(_cells) {
 
 // AIに考えてもらう
 function thinkAI(){
-    const hand = think(cells, -1, 5);
+    const hand = think(cells, -1, 9, mode === "easy");
     if (hand){
     document.querySelector(`#cell_${hand[0]}_${hand[1]}`);
     cell.textContent = " X ";
