f201e3e655ba48336b01fb11f2031a975c5288f1
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-09-14 12:48:01 +0900

    綴ミス等を修正

--- main.js
+++ main.js
@@ -20,11 +20,9 @@ for (let row = 0; row < 3; row++) {
             if (result !== CONTINUE) {
                 window.location.reload(true); // 決着がついた後にクリックしたらリロード
             }
-            if (cells[row][col === 0]){
-            putMark(row, col); // ○か×を置く
+            putMark(row,col);
             turn = turn * -1;
-            check(); // ゲームの状態を確認
-            }
+            check();
         });
     }
 }
@@ -35,12 +33,12 @@ function putMark(row, col) {
     if (turn === 1){
         cell.textContent = "o";
         cell.classList.add("o");
-        cells[row] = 1;
-[col]    } else {
-    cell.textConstent = "x";
-    cell.classList.add("x");
-    cells[row][col] = -1;
-}
+        cells[row][col] = 1;
+    } else {
+        cell.textContent = "x";
+        cell.classList.add("x");
+        cells[row][col] = -1;
+    }
 }
 
 // ゲームの状態を確認
